c39f43a82a87ed84a45e5ecbeb8fdebe016dda53
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-08-20 17:48:03 -0400

    migrate akdvantagekit helpers

---
+++ src/main/java/frc/minolib/advantagekit/AlertManager.java
@@ -0,0 +1,41 @@
+package frc.minolib.advantagekit;
+
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.RobotState;
+import frc.robot.Robot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AlertManager {
+  private static List<Alert> alerts = new ArrayList<>();
+
+  // this is a static class and cannot be instantiated without reflection
+  private AlertManager() {}
+
+  public static void registerAlert(Alert... alertsList) {
+    alerts.addAll(Arrays.asList(alertsList));
+  }
+
+  /**
+   * Updates all of the alerts in the manager. This must be called AFTER {@code
+   * CommandScheduler.getInstance().run()} in the robot periodic to work properly.
+   */
+  public static void update() {
+    boolean anyActive = false;
+    for (Alert alert : alerts) {
+      anyActive |= alert.get();
+    }
+
+    /* Need to fix this to register all alerts either in a RobotState or Robot
+
+    if (anyActive) {
+        Robot.getInstance().triggerAlert();
+    } else {
+        RobotState.getInstance().cancelAlert();
+    }
+
+      */
+  }
+}
