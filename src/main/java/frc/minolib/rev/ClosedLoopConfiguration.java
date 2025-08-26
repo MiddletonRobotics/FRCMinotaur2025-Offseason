4cab03730569d5e3e61d210b6f9cec10dcf26b5b
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-08-25 21:10:00 -0400

    rev pid controller (why is rev so wierd)

--- src/main/java/frc/minolib/rev/ClosedLoopConfiguration.java
+++ src/main/java/frc/minolib/rev/ClosedLoopConfiguration.java
@@ -3,64 +3,48 @@ package frc.minolib.rev;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.Slot2Configs;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.MAXMotionConfig;
+import com.revrobotics.spark.config.SmartMotionConfig;
+import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
 
 public class ClosedLoopConfiguration {
     public final double kP;
     public final double kI;
     public final double kD;
-    public final double kS;
-    public final double kV;
-    public final double kA;
-    public final double kG;
+    public final double mV;
+    public final double mA;
 
     public ClosedLoopConfiguration() {
-        this(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        this(0.0, 0.0, 0.0, 0.0, 0.0);
     }
 
     public ClosedLoopConfiguration(final double kP, final double kI, final double kD) {
-        this(kP, kI, kD, 0.0, 0.0, 0.0, 0.0);
+        this(kP, kI, kD, 0.0, 0.0);
+    }
+
+    public ClosedLoopConfiguration(final double mV, final double mA) {
+        this(0.0, 0.0,0.0, mV, mA);
     }
     
-  public ClosedLoopConfiguration(final double kP, final double kI, final double kD, final double kS, final double kV, final double kA, final double kG) {
+    public ClosedLoopConfiguration(final double kP, final double kI, final double kD, final double mV, final double mA) {
         this.kP = kP;
         this.kI = kI;
         this.kD = kD;
-        this.kS = kS;
-        this.kV = kV;
-        this.kA = kA;
-        this.kG = kG;
+        this.mV = mV;
+        this.mA = mA;
     }
 
-    public Slot0Configs fillCTRE(Slot0Configs conf) {
-        conf.kP = kP;
-        conf.kI = kI;
-        conf.kD = kD;
-        conf.kS = kS;
-        conf.kV = kV;
-        conf.kA = kA;
-        conf.kG = kG;
-        return conf;
+    public ClosedLoopConfig fillREV(ClosedLoopConfig configuration) {
+        configuration.p(kP);
+        configuration.i(kI);
+        configuration.d(kD);
+        return configuration;
     }
 
-    public Slot1Configs fillCTRE(Slot1Configs conf) {
-        conf.kP = kP;
-        conf.kI = kI;
-        conf.kD = kD;
-        conf.kS = kS;
-        conf.kV = kV;
-        conf.kA = kA;
-        conf.kG = kG;
-        return conf;
-    }
-
-    public Slot2Configs fillCTRE(Slot2Configs conf) {
-        conf.kP = kP;
-        conf.kI = kI;
-        conf.kD = kD;
-        conf.kS = kS;
-        conf.kV = kV;
-        conf.kA = kA;
-        conf.kG = kG;
-        return conf;
+    public MAXMotionConfig fillREV(MAXMotionConfig configuration) {
+        configuration.maxVelocity(mV);
+        configuration.maxAcceleration(mA);
+        return configuration;
     }
 }
