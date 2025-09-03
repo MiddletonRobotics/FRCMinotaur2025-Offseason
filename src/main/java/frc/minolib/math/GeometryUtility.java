30285f9f209b32384cfff2ae00458c813338fba6
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-09-02 22:47:37 -0400

    added more utilities

--- src/main/java/frc/minolib/math/GeometryUtility.java
+++ src/main/java/frc/minolib/math/GeometryUtility.java
@@ -168,4 +168,27 @@ public class GeometryUtility {
     public static Rotation3d constructRotation3d(Angle roll, Angle pitch, Angle yaw) {
         return new Rotation3d(roll, Degrees.zero(), Degrees.zero()).rotateBy(new Rotation3d(Degrees.zero(), pitch, Degrees.zero())).rotateBy(new Rotation3d(Degrees.zero(), Degrees.zero(), yaw));
     }
+
+    public static final Pose2d kPose2dZero = new Pose2d();
+
+    public static final Pose2d pose2dFromRotation(Rotation2d rotation) {
+        return new Pose2d(kTranslation2dZero, rotation);
+    }
+
+    public static final Pose2d pose2dFromTranslation(Translation2d translation) {
+        return new Pose2d(translation, kRotation2dZero);
+    }
+
+    public static final Rotation2d kRotation2dZero = new Rotation2d();
+    public static final Rotation2d kRotation2dPi = Rotation2d.fromDegrees(180.0);
+    public static final Translation2d kTranslation2dZero = new Translation2d();
+    public static final Transform2d kTransform2dZero = new Transform2d();
+
+    public static final Transform2d transform2dFromRotation(Rotation2d rotation) {
+        return new Transform2d(kTranslation2dZero, rotation);
+    }
+
+    public static final Transform2d transform2dFromTranslation(Translation2d translation) {
+        return new Transform2d(translation, kRotation2dZero);
+    }
 }
