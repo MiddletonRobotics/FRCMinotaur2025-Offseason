4836910fafac9834bdbaa9a5b10138bdeea1ffe8
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-18 22:50:04 -0500

    remodeled IO

--- src/main/java/frc/robot/subsystems/leds/LEDIO.java
+++ src/main/java/frc/robot/subsystems/leds/LEDIO.java
@@ -1,18 +1,20 @@
 package frc.robot.subsystems.leds;
 
-import frc.robot.subsystems.leds.LedState;
-
 import org.littletonrobotics.junction.AutoLog;
+
+import com.ctre.phoenix.led.Animation;
+
+import edu.wpi.first.wpilibj.util.Color;
 
 public interface LedIO {
     @AutoLog
     public class LedInputs {}
 
-    LedState getCurrentState();
+    default void setLEDs(Color color) {}
 
-    default void setLEDs(LedState wantedState) {}
+    default void setLEDs(int red, int green, int blue) {}
 
-    default void setLEDs(LedState[] wantedStates) {}
+    default void setAnimation(Animation animation) {}
 
     default void clearAnimation() {}
 }
